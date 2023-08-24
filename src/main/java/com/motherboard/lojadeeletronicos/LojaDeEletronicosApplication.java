907098108a82f0e445cdea666b467e7b15e0d29a
João Pedro da Silva Ferreira <rm86480@estudante.fieb.edu.br>
2023-08-24 08:56:28 -0300

    Criaçao do ambiente de desenvolvimento

---
+++ src/main/java/com/motherboard/lojadeeletronicos/LojaDeEletronicosApplication.java
@@ -0,0 +1,13 @@
+package com.motherboard.lojadeeletronicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeEletronicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeEletronicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motherboard/lojadeeletronicos/LojaDeEletronicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motherboard.lojadeeletronicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeEletronicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
